226523bf6e6009ac4599b40b8dd57344df55cf52
Gandhi Jigar <jigarr.gandhi@gmail.com>
2017-11-27 17:28:07 +0530

    Added UI for incrementing score

--- questions/static/js/question_detail.js
+++ questions/static/js/question_detail.js
@@ -1,4 +1,22 @@
-﻿function increment_score(question_id) {
+﻿$(function () {
+    BindEventListeners();
+});
+function BindEventListeners() {
+    $("#vote_question_up").click(function () {
+        $(this).addClass("vote_up vote_off");
+        var questionId = +$(this).data('questionid');
+        increment_score(questionId);
+    })
+    $("#vote_question_down").click()
+}
+function increment_score(question_id) {
 
-    
+    $.ajax({
+        method: 'POST',
+        url: '/questions/' + question_id + '/add_score'
+    }).success(function (e) {
+        console.log(e);
+    }).failure(function (e) {
+        console.error(e);
+    })
 }
